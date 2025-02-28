8a19bcd7a1fea23b8b167fb7f706fda855f58425
roopeshkumar11 <roopeshkumarbxr2017@gmail.com>
2025-03-01 00:04:33 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -10,8 +10,8 @@ dotenv.config({
 
 connectdb()
 .then(() => {
-    app.listen(process.env.port || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.port}`);
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
 .catch((err) => {
